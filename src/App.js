3187d770490eab687c6e57eb9455b8da05334ce9
Paula Krupa <paula.krupa@gmail.com>
2020-07-07 16:17:42 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,35 +1,53 @@
 import React from 'react';
 import './App.css';
-import './Colors.css';
-import One from "./Components/One";
-import Two from "./Components/Two";
 
-let blublu = prompt("Gimme number:");
+//this is a test of the way objects work, basically. Whether you can nest key value pairs in properties.
 
 class App extends React.Component {
 
     state = {
-        rotation: {}
-    };
+        obsession: {
+            title: "Chén Qíng Lìng",
+            favorites: {
+                character: ["Nie Huaisang", "Lan Xichen", "Jin Guangyao", "Jiang Cheng"],
+                ship: ["Xiyao", "Sangcheng", "Xisang"],
+                sect: ["Gusu Lan", "Yunmeng Jiang"]
+            },
+            leastFavorites: {
+                character: "Jin Guangshan",
+                ship: "Songxiao",
+                sect: "Jin"
+            }
+        }
+    }
 
-    handleClick = () => {
-        this.setState({rotation: this.rotation})
-    };
+    //nie można dwóch elementów przypisać do jednego key? trzeba je włożyć w array, jeśli mam na to ochotę?
+    //nie można używać myślników w nazwach keys?
 
-    rotation = {
-        transform: "rotate(" + blublu*30 + "deg)"
-    };
+    //should there be a way to make these things smaller?
 
     render() {
         return (
             <div>
-                <One rotation={this.state.rotation}/>
-                <Two
-                    handleClick={this.handleClick}
-                />
+                Hi, my recent obsession is {this.state.obsession.title}.<br/>
+                My favorite character is {this.state.obsession.favorites.character[0]}, although I love them only a
+                little bit more than {this.state.obsession.favorites.character
+                .slice(1, this.state.obsession.favorites.character.length - 1)
+                .join(", ")} and {this.state.obsession.favorites.character[this.state.obsession.favorites.character.length - 1]}.<br/>
+                My favorite ships
+                are {this.state.obsession.favorites.ship.slice(0, this.state.obsession.favorites.ship.length - 1).join(", ")} and {this.state.obsession.favorites.ship[this.state.obsession.favorites.ship.length - 1]},
+                but I'll take anything with {this.state.obsession.favorites.character[0]}.<br/>
+                My favorite sect is {this.state.obsession.favorites.sect[0]},
+                although {this.state.obsession.favorites.sect[1]} probably fits me better.
+                <br/>
+                <br/>
+                My least favorite character is {this.state.obsession.leastFavorites.character}.<br/>
+                My least favorite ship is {this.state.obsession.leastFavorites.ship}.<br/>
+                My least favorite sect is {this.state.obsession.leastFavorites.sect}.<br/>
+                <br/>
+                As is evident, I love much more than I dislike about the world of {this.state.obsession.title}.
             </div>
         )
-
     }
 }
 
